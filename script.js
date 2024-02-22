6f5c21a06050c498b6664b0981accf635d3f8dcf
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-22 15:12:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//your JS code here. If required.
+const tbody = document.querySelector('.tbody');
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const title = form.title.value;
+    const author = form.author.value;
+    const isbn = form.isbn.value;
+
+    const tr = document.createElement('tr');
+    const userTitle = document.createElement('td');
+    const userAuthor = document.createElement('td');
+    const userIsbn = document.createElement('td');
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.textContent = "X";
+
+    userTitle.textContent = title;
+    userAuthor.textContent = author;
+    userIsbn.textContent = isbn;
+
+    tr.appendChild(userTitle);
+    tr.appendChild(userAuthor);
+    tr.appendChild(userIsbn);
+    tr.appendChild(deleteBtn);
+
+    tbody.appendChild(tr);
+	form.reset();
+    deleteBtn.addEventListener('click', () => {
+        tr.remove();
+    });
+});
